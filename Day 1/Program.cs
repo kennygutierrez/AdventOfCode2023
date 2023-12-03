2d44b7beed791d887fbdeab1380750a556c9c57f
Kenny Gutierrez <kenny.gutierrez@centurycommunities.com>
2023-12-03 08:18:49 -0600

    Add project files.

---
+++ Day 1/Program.cs
@@ -0,0 +1,83 @@
+﻿var lines = File.ReadAllLines(@"C:\Users\kenny.gutierrez\source\repos\Advent Of Code\Day 1\input.txt");
+
+var total = 0;
+
+foreach (var line in lines)
+{
+    var chars = new char[2];
+
+    foreach (var ch in line.Where(char.IsNumber))
+    {
+        chars[0] = ch;
+        break;
+    }
+
+    foreach (var ch in line.Reverse())
+    {
+        if (char.IsNumber(ch))
+        {
+            chars[1] = ch;
+            break;
+        }
+    }
+
+    var twoDigitInt = int.Parse(new string(chars));
+
+    total += twoDigitInt;
+}
+
+Console.WriteLine($"Part 1 Grand total is {total}");
+
+//part 2
+
+var partTwoTotal = 0;
+
+foreach (var line in lines)
+{
+    Dictionary<int, int> matches = new();
+
+    var numbersArray1 = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    var numbersArray2 = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+    for (int i = 0; i < numbersArray1.Length; i++)
+    {
+        if (line.Contains(numbersArray1[i]))
+        {
+            AddMatches(matches, line, numbersArray1[i], i+1);
+        }
+    }
+
+    for (int i = 0; i < numbersArray2.Length; i++)
+    {
+        if (line.Contains(numbersArray2[i]))
+        {
+            AddMatches(matches, line, numbersArray2[i], i + 1);
+        }
+    }
+
+    var firstNumberMatch = matches.MinBy(x => x.Key);
+    var firstNumber = firstNumberMatch.Value;
+
+    var secondNumberMatch = matches.MaxBy(x => x.Key);
+    var secondNumber = secondNumberMatch.Value;
+
+    var twoDigitInt = int.Parse(firstNumber + secondNumber.ToString());
+
+    partTwoTotal += twoDigitInt;
+
+}
+
+Console.WriteLine($"Part 2 Grand total is {partTwoTotal}");
+return;
+
+static void AddMatches(IDictionary<int, int> matches, string line, string searchTerm, int numberTerm)
+{
+    var foundAt = line.IndexOf(searchTerm, StringComparison.Ordinal);
+    matches.Add(foundAt, numberTerm);
+
+    if (line.LastIndexOf(searchTerm, StringComparison.Ordinal) > foundAt)
+    {
+        matches.Add(line.LastIndexOf(searchTerm, StringComparison.Ordinal), numberTerm);
+    }
+}
---
+++ Day 2/Program.cs
@@ -0,0 +1,72 @@
+﻿var games = File.ReadAllLines(@"C:\Users\kenny.gutierrez\source\repos\Advent Of Code\Day 2\input.txt");
+
+const int redCount = 12;
+const int greenCount = 13;
+const int blueCount = 14;
+
+var possibleCount = 0;
+var totalPower = 0;
+
+foreach (var game in games)
+{
+    var roundNum = Int32.Parse(game.Split(':')[0].Replace("Game ", string.Empty));
+
+    var results = game.Split(':')[1];
+
+    var rounds = results.Split(';');
+
+    var possible = false;
+
+    foreach (var round in rounds)
+    {
+        var colorCounts = round.Split(",");
+
+        int.TryParse( colorCounts.FirstOrDefault(x => x.Contains("red"))?.Replace(" red", string.Empty), out var redResults);
+        int.TryParse(colorCounts.FirstOrDefault(x => x.Contains("blue"))?.Replace(" blue", string.Empty), out var blueResults);
+        int.TryParse(colorCounts.FirstOrDefault(x => x.Contains("green"))?.Replace(" green", string.Empty), out var greenResults);
+
+        possible = redResults <= redCount && blueResults <= blueCount && greenResults <= greenCount;
+
+        if (!possible)
+        {
+            break;
+        }
+    }
+
+    if (possible)
+    {
+        possibleCount += roundNum;
+    }
+}
+
+Console.WriteLine($"Sum of Game Ids is {possibleCount}");
+
+//part 2
+
+foreach (var game in games)
+{
+    var results = game.Split(':')[1];
+
+    var rounds = results.Split(';');
+
+    var highestRed = 0;
+    var highestGreen = 0;
+    var highestBlue = 0;
+
+    foreach (var round in rounds)
+    {
+        string[] colorCounts = round.Split(",");
+
+        int.TryParse(colorCounts.FirstOrDefault(x => x.Contains("red"))?.Replace(" red", string.Empty), out var redResults);
+        int.TryParse(colorCounts.FirstOrDefault(x => x.Contains("blue"))?.Replace(" blue", string.Empty), out var blueResults);
+        int.TryParse(colorCounts.FirstOrDefault(x => x.Contains("green"))?.Replace(" green", string.Empty), out var greenResults);
+
+        if (redResults > highestRed) { highestRed = redResults; }
+        if (greenResults > highestGreen) { highestGreen = greenResults; }
+        if (blueResults > highestBlue) { highestBlue = blueResults; }
+    }
+
+    totalPower += highestBlue * highestGreen * highestRed;
+}
+
+Console.WriteLine($"Total power is {totalPower}");
