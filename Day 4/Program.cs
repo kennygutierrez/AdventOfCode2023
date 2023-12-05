6e2a7220303fded9ed032e5261329ef1e7361865
Kenny Gutierrez <kenny.gz@gmail.com>
2023-12-05 00:11:02 -0600

    attempt at part 2

--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -1,22 +1,27 @@
-﻿namespace Day_4
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines(@"C:\Users\kenny.gutierrez\source\repos\Advent Of Code\Day 4\input.txt");
+            var lines = File.ReadAllLines(@"C:\Users\kenny\Source\Repos\AdventOfCode2023\Day 4\input.txt");
+
+            var grandTotal = 0;
 
             foreach (var line in lines)
             {
                 var numbers = line.Split(':');
-                var winningNumbers = numbers[1].Replace("  ", " ").Split('|')[0];
-                var myNumbers = numbers[1].Replace("  ", " ").Split('|')[1];
+                var winningNumbers = numbers[1].Trim().Replace("  ", " ").Split('|')[0];
+                var myNumbers = numbers[1].Trim().Replace("  ", " ").Split('|')[1];
 
                 var matches = 0;
-                var grandTotal = 0;
 
-                var individualWinningNumbers = winningNumbers.Split(" ").ToList();
-                var individualMyNumbers = myNumbers.Split(" ").ToList();
+
+                var individualWinningNumbers = winningNumbers.Trim().Split(" ").ToList();
+                var individualMyNumbers = myNumbers.Trim().Split(" ").ToList();
 
                 foreach (var winningNumber in individualWinningNumbers)
                 {
@@ -25,22 +30,66 @@
                         matches++;
                     }
                 }
-
-                Console.WriteLine($"Matches: {matches}, Points: {points(matches)}");
-                Console.WriteLine($"Grand Total: {grandTotal}");
+                grandTotal += points(matches);
             }
+            Console.WriteLine($"Grand Total: {grandTotal}");
+
+            //part 2
+
+            var totalCards = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int cards = ProcessCard(lines, i, 0);
+
+                totalCards = totalCards + cards;
+            }
+            Console.WriteLine($"Total Cards: {totalCards}");
         }
 
-        private static long points(int n)
+        private static int ProcessCard(string[] lines, int index, int copies)
         {
-            var points = 0;
+            
+            Console.WriteLine(lines[index]);
+
+            if (index + 1 == lines.Length)
+            {
+                return copies + 1;
+            }
+
+            var line = lines[index];
+
+            var numbers = line.Split(':');
+            var winningNumbers = numbers[1].Trim().Replace("  ", " ").Split('|')[0];
+            var myNumbers = numbers[1].Trim().Replace("  ", " ").Split('|')[1];
+
+            var individualWinningNumbers = winningNumbers.Trim().Split(" ").ToList();
+            var individualMyNumbers = myNumbers.Trim().Split(" ").ToList();
+
+            foreach (var winningNumber in individualWinningNumbers)
+            {
+
+                if (individualMyNumbers.Contains(winningNumber))
+                {
+                    copies++;
+                }
+            }
+
+            return ProcessCard(lines, index + 1, copies + 1);
+        }
+
+        private static int points(int n)
+        {
+            if (n == 0) return 0;
+
+            var points = 1;
 
             for (int i = 1; i < n; i++)
             {
-                points = i * 2;
+                points = points * 2;
             }
 
-            return points + 1;
+            return points;
         }
     }
 }
